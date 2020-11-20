aba32395ef69b03370a20c7a2128697212f5eae9
filishere <71631197+filishere@users.noreply.github.com>
2020-11-20 14:32:42 +0200

    Add files via upload

---
+++ MatrixSum.java
@@ -0,0 +1,45 @@
+package homework;
+import java.util.Scanner;
+public class MatrixSum {
+	public static void main(String[] args) {
+		final int arraySize =3;
+		int [][] matrix = new int [arraySize][arraySize];
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i < arraySize; i++) {
+			for(int j = 0; j < arraySize ; j++) {
+				System.out.println("Enter a number on index " + i + ", " + j);
+				matrix[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int i = 0; i < arraySize; i++) {
+			for(int j = 0; j < arraySize ; j++) {
+				System.out.print(matrix[i][j] + "\t");
+			}
+			System.out.println();
+			}
+		sc.close();
+		int indexMaxSum = 0, maxSum =0;
+		for(int i = 0; i < arraySize; i++) {
+			int sumCurrentRow = 0;
+			for(int j = 0; j < arraySize ; j++) {
+				sumCurrentRow += matrix[i][j];
+			}
+			if (i == 0) {
+				indexMaxSum = 1;
+				maxSum = sumCurrentRow;
+			} else if ( sumCurrentRow > maxSum)  {
+				indexMaxSum = i;
+				maxSum = sumCurrentRow;
+			}
+		}
+		
+		System.out.println("The row with the biggest sum of elements is with index " + indexMaxSum);
+		
+	}
+	
+	
+	
+	
+	
+}
